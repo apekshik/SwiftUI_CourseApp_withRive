f7700ff7d221e5a9d561bbd1dfec0f60cd908147
Apekshik Panigrahi <apekshik@gmail.com>
2022-05-31 10:18:20 +0530

    Initial Commit

---
+++ Swift_RiveAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift_RiveAnimation
+//
+//  Created by Apekshik Panigrahi on 5/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift_RiveAnimation/Swift_RiveAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_RiveAnimationApp.swift
+//  Swift_RiveAnimation
+//
+//  Created by Apekshik Panigrahi on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_RiveAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
